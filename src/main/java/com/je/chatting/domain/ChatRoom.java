374adeedfac7fc22db3216881d5ad38b94439ec4
3jieun3 <skysun704@gmail.com>
2022-11-07 02:37:28 +0900

    web socket chatting using STOMP (1-1)

--- src/main/java/com/je/chatting/domain/ChatRoom.java
+++ src/main/java/com/je/chatting/domain/ChatRoom.java
@@ -20,19 +20,19 @@ public class ChatRoom {
         this.name = name;
     }
 
-//    private final Set<WebSocketSession> sessionList = new HashSet<>();  // 채팅방에 접속 중인 세션 리스트
+    private final Set<WebSocketSession> sessionList = new HashSet<>();  // 채팅방에 접속 중인 세션 리스트
 
-//    public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService) {
-//        if (chatMessage.getType().equals(ChatMessage.MessageType.JOIN)) {           /* 채팅방 접속 시 */
-//            log.info("{} join \"{}\"!", session.getId(), name);
-//            sessionList.add(session);
-//            chatMessage.setContent(String.format("%s JOIN '%s'!", chatMessage.getSender(), name));
-//        } else if (chatMessage.getType().equals(ChatMessage.MessageType.LEAVE)) {   /* 채팅방 접속 해제 시 */
-//            log.info("{} leave \"{}\"!", session.getId(), name);
-//            sessionList.remove(session);
-//            chatMessage.setContent(String.format("%s LEAVE '%s'!", chatMessage.getSender(), name));
-//        }
-//        // 채팅방의 모든 세션에 메시지 전송
-//        sessionList.parallelStream().forEach(sess -> chatService.sendMessage(sess, chatMessage));
-//    }
+    public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService) {
+        if (chatMessage.getType().equals(ChatMessage.MessageType.JOIN)) {           /* 채팅방 접속 시 */
+            log.info("{} join \"{}\"!", session.getId(), name);
+            sessionList.add(session);
+            chatMessage.setContent(String.format("%s JOIN '%s'!", chatMessage.getSender(), name));
+        } else if (chatMessage.getType().equals(ChatMessage.MessageType.LEAVE)) {   /* 채팅방 접속 해제 시 */
+            log.info("{} leave \"{}\"!", session.getId(), name);
+            sessionList.remove(session);
+            chatMessage.setContent(String.format("%s LEAVE '%s'!", chatMessage.getSender(), name));
+        }
+        // 채팅방의 모든 세션에 메시지 전송
+        sessionList.parallelStream().forEach(sess -> chatService.sendMessage(sess, chatMessage));
+    }
 }
